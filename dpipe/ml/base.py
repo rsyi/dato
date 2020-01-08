eb1b6b127c3d6272dcf0e25772bd6eaabb4c5dee
Robert Yi <robert@ryi.me>
2020-01-07 22:13:20 -0800

    Restructure ML-related functions to use accumulator for all ML-related tasks.

--- dpipe/ml/base.py
+++ dpipe/ml/base.py
@@ -10,40 +10,53 @@ def _print_regression_error(y_true, y_pred):
 
 
 class _ModelSpec():
-    def __init__(self, dpipe_output, estimator=None, **kwargs):
+    def __init__(self, df):
+        self.data = df
+        self.original_data = df
+        self.train = df
+        self.X = self.data.iloc[:,:-1]
+        self.y = self.data.iloc[:,-1]
+        self.X_train = self.X.copy()
+        self.y_train = self.y.copy()
+        self.X_test = None
+        self.y_test = None
+        self.encoders = {}
 
-        # Handle if train test split has been used (and so the input is a two-element list).
-        if type(dpipe_output) == list:
-            self.train = dpipe_output[0]
-            self.test = dpipe_output[1]
-            self.test_X = self.test.iloc[:,:-1]
-            self.test_y = self.test.iloc[:,-1]
-        else:
-            self.train = dpipe_output[0]
-            self.test = None
+    def train_test_split(self, X, y, **kwargs):
+        self.X_train, self.X_test, self.y_train, self.y_test = skl.model_selection.train_test_split(X, y, **kwargs)
+        return self.X_train, self.X_test, self.y_train, self.y_test
 
-        self.train_X = self.train.iloc[:,:-1]
-        self.train_y = self.train.iloc[:,-1]
-        self.estimator = estimator(**kwargs)
+    def encode(self, Encoder, columns):
+        enc_dict = {}
+        for column in columns:
+            enc = Encoder()
+            self.data[column] = enc.fit_transform(self.data[column])
+            enc_dict[column] = enc
+        self.encoders.update(enc_dict)
+
+        return encoded_df, enc_dict
+
+    def instantiate_estimator(self, Estimator, **kwargs):
+        self.estimator = Estimator(**kwargs)
 
     def predict(self):
-        self.train_y_prediction = self.estimator.predict(self.train_X)
-        if self.test is not None:
-            self.test_y_prediction = self.estimator.predict(self.test_X)
+        self.train_y_prediction = self.estimator.predict(self.X_train)
+        if self.y_test is not None:
+            self.test_y_prediction = self.estimator.predict(self.X_test)
 
     def evaluate(self, kind='regressor'):
         if kind=='regressor':
-            if self.test is not None:
+            if self.y_test is not None:
                 print('\033[1mTraining set')
                 print(len('Training set')*'-','\033[0m')
-            _print_regression_error(self.train_y, self.train_y_prediction)
+            _print_regression_error(self.y_train, self.train_y_prediction)
             print('\n')
-            if self.test is not None:
+            if self.y_test is not None:
                 print('\033[1mTest set')
                 print(len('Test set')*'-','\033[0m')
-                _print_regression_error(self.test_y, self.test_y_prediction)
+                _print_regression_error(self.y_test, self.test_y_prediction)
         elif kind=='classifier':
-            if self.test is not None:
+            if self.y_test is not None:
                 # Plot roc auc curve.
                 pass
 
